0b93879250da3d336e1c2fc3f7162fcef369b098
单国力 <shanguoli@em-data.com.cn>
2018-09-27 17:10:53 +0800

    configurable

--- src/index.js
+++ src/index.js
@@ -1,22 +1,43 @@
-var addEvent = function (el, capture,needBar) {
+/*
+* @self
+* dom dom节点
+* capture 事件捕获
+* needBar 是否需要滚动轴
+* barColor 滚动轴颜色
+* barbgColor 滚动轴背景颜色
+*/
 
+function MyScrollbar (args) {
+
+    var el = args.dom || null
+    var capture = args.capture || false
+    var needBar = args.needBar || false
+    var barColor = args.barColor || '#ff6600'
+    var barbgColor = args.barbgColor || 'rgba(0,0,0,.1)'
+
+    var self = this
     this.sy = ''    //鼠标位置Y
     this.ost = ''   //滚动轴位置
     this.movey = '' // y轴运动距离
     this.afterCpTop = ''  // 滚动轴计算后位置
-    var self = this
     if(!el){
         throw Error('no found element')
     }
     var elClass = el.getAttribute('class')
     el.setAttribute('class',elClass + ' sg-scrollbar')
     var content = el.querySelector('.content')
+    if(!content){
+        throw Error('please add the class name "content" to your child element ')
+    }
+
     // 生成滚动轴
     if(needBar){
         var barContainer = document.createElement('div')
         var bar = document.createElement('div')
         barContainer.setAttribute('class','scrollbar')
+        barContainer.style.backgroundColor = barbgColor
         bar.setAttribute('class','bar')
+        bar.style.backgroundColor = barColor
         barContainer.appendChild(bar)
         el.appendChild(barContainer)
         var viewh = el.offsetHeight
@@ -29,10 +50,8 @@ var addEvent = function (el, capture,needBar) {
     }
     el.addEventListener(type, function (event) {
         event.stopPropagation()
-        var viewh = el.offsetHeight
-        var totalh = content.offsetHeight
-        var ph = 100 //(totalh - viewh) / 4
-        var ev = _eventCompat(event)
+        var ph = 100 
+        var ev = self.mouseEventCompat(event)
         if (ev.delta > 0) {
             el.scrollTop -= ph                        
         } else {
@@ -63,7 +82,7 @@ var addEvent = function (el, capture,needBar) {
                 
                 bar.style.top = self.afterCpTop + 'px'
                 el.scrollTop = self.afterCpTop*totalh/viewh
-                barContainer.style.top = el.scrollTop
+                barContainer.style.top = el.scrollTop + 'px'
             }
             
         }
@@ -71,21 +90,26 @@ var addEvent = function (el, capture,needBar) {
             self.sy = '' 
         }
     }
+    this.mouseEventCompat = function(event){
+        var type = event.type
+        if (type === 'DOMMouseScroll' || type === 'mousewheel') {
+            event.delta = (event.wheelDelta) ? event.wheelDelta / 120 : -(event.detail || 0) / 3
+        }
+        if (event.srcElement && !event.target) {
+            event.target = event.srcElement
+        }
+        if (!event.preventDefault && event.returnValue !== undefined) {
+            event.preventDefault = function () {
+            event.returnValue = false
+            }
+        }
+        return event
+    }
     
 }
-var _eventCompat = function (event) {
-    var type = event.type
-    if (type === 'DOMMouseScroll' || type === 'mousewheel') {
-        event.delta = (event.wheelDelta) ? event.wheelDelta / 120 : -(event.detail || 0) / 3
-    }
-    if (event.srcElement && !event.target) {
-        event.target = event.srcElement
-    }
-    if (!event.preventDefault && event.returnValue !== undefined) {
-        event.preventDefault = function () {
-        event.returnValue = false
-        }
-    }
-    return event
-}
-addEvent(document.querySelector('.dd'), false,true)
+// var _eventCompat = function (event) 
+new MyScrollbar({
+    dom:document.querySelector('.dd'),
+    capture:false,
+    needBar:true
+})
